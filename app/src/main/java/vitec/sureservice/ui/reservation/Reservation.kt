5ac15210adc1882218100e461d349a9187987402
Jefferson Ortega <71106231+JeffersonOrtega2018@users.noreply.github.com>
2022-10-18 12:28:57 -0500

    Feat: Add new images

--- app/src/main/java/vitec/sureservice/ui/reservation/Reservation.kt
+++
@@ -1,9 +0,0 @@
-package vitec.sureservice.ui.reservation
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun Reservation() {
-    Text(text = "Reservation")
-}
